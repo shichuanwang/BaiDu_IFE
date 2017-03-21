b6445fd9ba541972c967447ed63c9b18eaf4261e
wangsirc <wangsirc@bicitech.cn>
2017-03-21 15:55:56 +0800

    ddd

--- js/public.js
+++ js/public.js
@@ -6,29 +6,33 @@
     // 设置导航条
     var nav = "<div style='width:1000px;margin:0px auto;'>";
     var headerNav = document.getElementById("header-nav");
-    for(var n=0; n<G_navData.length; n++) {
-        nav += "<p style='"+styles.navline+"' >"+
-                    "<a href='javascirpt:void(0);' data-type='"+G_navData[n].navType+"' onclick='selectSourceType(this)'>"+G_navData[n].text+"</a>"+
-               "</p>";
+    if( headerNav ) {
+        for(var n=0; n<G_navData.length; n++) {
+            nav += "<p style='"+styles.navline+"' >"+
+                        "<a href='javascirpt:void(0);' data-type='"+G_navData[n].navType+"' onclick='selectSourceType(this)'>"+G_navData[n].text+"</a>"+
+                "</p>";
+        }
+        headerNav.innerHTML = nav+"</div>";
     }
-    headerNav.innerHTML = nav+"</div>";
 
     // content-left 内容添加
     var contentaside = document.getElementById("content-left"),
         showAside = "", linedata, line;
-    for(var i=0; i<G_webSource.length; i++) {
-        showAside += "<div style='"+styles.webfont.wrapper+"'><div style='"+styles.webfont.title+"'>"+G_webSource[i].title+"</div>";
-        for(var j=0; j<G_webSource[i].content.length; j++){
-            linedata = G_webSource[i].content[j];
-            showAside += "<p style='"+styles.webfont.type+"'>"+linedata.type+"</p>";
-            for(var k=0; k<linedata.webfont.length; k++) {
-                line = linedata.webfont[k];
-                showAside += "<span style='"+styles.webfont.webfont+"'><a href='"+line.url+"' title='"+line.discrible+"'>"+line.name+"</a></span>";
+    if( contentaside ) {
+        for(var i=0; i<G_webSource.length; i++) {
+            showAside += "<div style='"+styles.webfont.wrapper+"'><div style='"+styles.webfont.title+"'>"+G_webSource[i].title+"</div>";
+            for(var j=0; j<G_webSource[i].content.length; j++){
+                linedata = G_webSource[i].content[j];
+                showAside += "<p style='"+styles.webfont.type+"'>"+linedata.type+"</p>";
+                for(var k=0; k<linedata.webfont.length; k++) {
+                    line = linedata.webfont[k];
+                    showAside += "<span style='"+styles.webfont.webfont+"'><a href='"+line.url+"' title='"+line.discrible+"'>"+line.name+"</a></span>";
+                }
             }
+            showAside += "</div>";
         }
-        showAside += "</div>";
+        contentaside.innerHTML = showAside;
     }
-    contentaside.innerHTML = showAside;
 
 
     // 设置IFE 任务显示图片 
@@ -51,6 +55,109 @@
         }
     }
 
+})()
+
+// 选择导航内容
+function selectSourceType(obj){
+    var showContent = "<div>",
+        sourceType = (typeof obj == "string") ? window.localStorage.getItem("navtype"):obj.getAttribute("data-type"),
+        sourceData = G_contentSource[sourceType],
+        fenYeDiv = document.getElementById("content-modle-fenye"),
+        dataLength = 0,
+        contentModle = document.getElementById("content-modle-showcontent");
+    if( sourceType ) {
+        // 设置主区内容分页
+        if(sourceData.length < G_PAGECOUNT+1) {
+            fenYeDiv.innerHTML = "<span id='pageCount'>共1页</span>"+
+                            "<span id='beforePage' class='beforepage-false'>上一页</span>"+
+                            "<span id='nowPage' style='color:blue;'>第1页</span>"+
+                            "<span id='nextPage' class='nextpage-false'>下一页</span>";
+            dataLength = sourceData.length;
+        } else if( sourceData.length > G_PAGECOUNT ) {
+            fenYeDiv.innerHTML = "<span id='pageCount'>共"+Math.ceil(sourceData.length/G_PAGECOUNT)+"页</span>"+
+                            "<span id='beforePage' class='beforepage-ture' onclick=\"fenye( '"+sourceType+"', 'pre')\">上一页</span>"+
+                            "<span id='nowPage' style='color:blue;' data-nowPage='1'>第1页</span>"+
+                            "<span id='nextPage'  class='nextpage-true' onclick=\"fenye( '"+sourceType+"', 'next')\">下一页</span>";
+            dataLength = G_PAGECOUNT;
+        }
+        // 设置主区内容显示
+        for(var i=0; i<dataLength; i++) {
+            showContent += "<p class='"+sourceData[i].titleClass+"'><i style='color:#9dccb6;font-size:14px;'>["+sourceData[i].type+"]</i>. "+sourceData[i].title+"</p>"+
+                        "<p class='"+sourceData[i].contentClass+"'>"+sourceData[i].discription+
+                                " <a href='"+sourceData[i].linkPath+"' class='"+sourceData[i].aStyle+"'>更多>></a>"+
+                            "</p>";
+        }
+        showContent += "</div>";
+        contentModle.innerHTML = showContent;
+        window.localStorage.setItem("navtype", sourceType)
+    }
+}
+
+// data 数据源 ， page 当前页面， dict 切换方向
+function fenye( sourcetype, dict){
+    var nowPage = document.getElementById("nowPage"),
+        page = parseInt(nowPage.getAttribute("data-nowPage")),
+        contentModle = document.getElementById("content-modle-showcontent"),
+        data = G_contentSource[sourcetype],
+        showContent = "<div>",
+        starNum = 0, endNum = data.length;
+    if( nowPage ) {
+        if( dict == "pre") {
+            if(page > 1 ) {
+                page = page - 1;
+                nowPage.innerHTML = "第"+page+"页";
+                nowPage.setAttribute("data-nowPage",page);
+            }
+        }else if( dict == "next") {
+            if(page < Math.ceil(data.length/G_PAGECOUNT) ) {
+                page = page + 1;
+                nowPage.innerHTML = "第"+page+"页";
+                nowPage.setAttribute("data-nowPage",page);
+                
+            }
+        }
+        starNum = (page-1)*G_PAGECOUNT;
+        endNum = (data.length-starNum > G_PAGECOUNT) ? (starNum+G_PAGECOUNT):  data.length;
+        for(var i= starNum; i< endNum; i++) {
+            showContent += "<p class='"+data[i].titleClass+"'><i style='color:#9dccb6;font-size:14px;'>["+data[i].type+"]</i>. "+data[i].title+"</p>"+
+                        "<p class='"+data[i].contentClass+"'>"+data[i].discription+
+                                "... <a href='"+data[i].linkPath+"' class='"+data[i].aStyle+"'>更多>></a>"+
+                            "</p>";
+        }
+        showContent += "</div>";
+        contentModle.innerHTML = showContent;
+    }
+}
+
+
+// 给代码片段写入内容的共用方法
+// snippet 翻译为: 代码段
+// snippetdata 的数据格式如下:
+//  snippetdata = {
+//    "data-datasource的属性值" :["test","",""]
+//  }
+function setSnippetContext( snippetdata ) {
+    var snippetTag = document.getElementsByClassName("snippet"),
+        datasource = "",
+        lineDataSource = [],
+        content = "";
+    if( snippetTag ) {
+        for(var i=0; i<snippetTag.length; i++) {
+            datasource = snippetTag[i].getAttribute("data-datasource");
+            if(datasource && snippetdata[datasource]) {
+                lineDataSource = snippetdata[datasource];
+                content = "<table>";
+                for(var j=0; j<lineDataSource.length; j++) {
+                    content += "<tr><td class='snippet-order' type='"+(j+1)+"'>"+(j+1)+"</td>"+ 
+                                    "<td class='snippet-content' type='"+(j+1)+"'>"+lineDataSource[j]+"</td>"+
+                            "</tr>";
+                }
+                content +="</table>";
+                snippetTag[i].innerHTML = content;
+            }
+        } 
+    }
+    
 
     // load代码片段
     var snippetContent = document.getElementsByClassName("snippet-content");
@@ -68,102 +175,10 @@
                 snippetContent[i].innerHTML = text.replace(dictText, "<i style='color:#a0d2ec;'>function</i>");
             }
 
-        }
-    }
-})()
-
-// 选择导航内容
-function selectSourceType(obj){
-    var showContent = "<div>",
-        sourceType = (typeof obj == "string") ? window.localStorage.getItem("navtype"):obj.getAttribute("data-type"),
-        sourceData = G_contentSource[sourceType],
-        fenYeDiv = document.getElementById("content-modle-fenye"),
-        dataLength = 0,
-        contentModle = document.getElementById("content-modle-showcontent");
-    // 设置主区内容分页
-    if(sourceData.length < G_PAGECOUNT+1) {
-        fenYeDiv.innerHTML = "<span id='pageCount'>共1页</span>"+
-                        "<span id='beforePage' class='beforepage-false'>上一页</span>"+
-                        "<span id='nowPage' style='color:blue;'>第1页</span>"+
-                        "<span id='nextPage' class='nextpage-false'>下一页</span>";
-        dataLength = sourceData.length;
-    } else if( sourceData.length > G_PAGECOUNT ) {
-        fenYeDiv.innerHTML = "<span id='pageCount'>共"+Math.ceil(sourceData.length/G_PAGECOUNT)+"页</span>"+
-                        "<span id='beforePage' class='beforepage-ture' onclick=\"fenye( '"+sourceType+"', 'pre')\">上一页</span>"+
-                        "<span id='nowPage' style='color:blue;' data-nowPage='1'>第1页</span>"+
-                        "<span id='nextPage'  class='nextpage-true' onclick=\"fenye( '"+sourceType+"', 'next')\">下一页</span>";
-        dataLength = G_PAGECOUNT;
-    }
-    // 设置主区内容显示
-    for(var i=0; i<dataLength; i++) {
-        showContent += "<p class='"+sourceData[i].titleClass+"'><i style='color:#9dccb6;font-size:14px;'>["+sourceData[i].type+"]</i>. "+sourceData[i].title+"</p>"+
-                       "<p class='"+sourceData[i].contentClass+"'>"+sourceData[i].discription+
-                            " <a href='"+sourceData[i].linkPath+"' class='"+sourceData[i].aStyle+"'>更多>></a>"+
-                        "</p>";
-    }
-    showContent += "</div>";
-    contentModle.innerHTML = showContent;
-    window.localStorage.setItem("navtype", sourceType)
-}
-
-// data 数据源 ， page 当前页面， dict 切换方向
-function fenye( sourcetype, dict){
-    var nowPage = document.getElementById("nowPage"),
-        page = parseInt(nowPage.getAttribute("data-nowPage")),
-        contentModle = document.getElementById("content-modle-showcontent"),
-        data = G_contentSource[sourcetype],
-        showContent = "<div>",
-        starNum = 0, endNum = data.length;
-    if( dict == "pre") {
-        if(page > 1 ) {
-            page = page - 1;
-            nowPage.innerHTML = "第"+page+"页";
-            nowPage.setAttribute("data-nowPage",page);
-        }
-    }else if( dict == "next") {
-        if(page < Math.ceil(data.length/G_PAGECOUNT) ) {
-            page = page + 1;
-            nowPage.innerHTML = "第"+page+"页";
-            nowPage.setAttribute("data-nowPage",page);
-            
-        }
-    }
-    starNum = (page-1)*G_PAGECOUNT;
-    endNum = (data.length-starNum > G_PAGECOUNT) ? (starNum+G_PAGECOUNT):  data.length;
-    for(var i= starNum; i< endNum; i++) {
-        showContent += "<p class='"+data[i].titleClass+"'><i style='color:#9dccb6;font-size:14px;'>["+data[i].type+"]</i>. "+data[i].title+"</p>"+
-                       "<p class='"+data[i].contentClass+"'>"+data[i].discription+
-                            "... <a href='"+data[i].linkPath+"' class='"+data[i].aStyle+"'>更多>></a>"+
-                        "</p>";
-    }
-    showContent += "</div>";
-    contentModle.innerHTML = showContent;
-}
-
-
-// 给代码片段写入内容的共用方法
-// snippet 翻译为: 代码段
-// snippetdata 的数据格式如下:
-//  snippetdata = {
-//    "data-datasource的属性值" :["test","",""]
-//  }
-function setSnippetContext( snippetdata ) {
-    var snippetTag = document.getElementsByClassName("snippet"),
-        datasource = "",
-        lineDataSource = [],
-        content = "";
-    for(var i=0; i<snippetTag.length; i++) {
-        datasource = snippetTag[i].getAttribute("data-datasource");
-        if(datasource && snippetdata[datasource]) {
-            lineDataSource = snippetdata[datasource];
-            content = "<table>";
-            for(var j=0; j<lineDataSource.length; j++) {
-                content += "<tr><td class='snippet-order' type='"+(j+1)+"'>"+(j+1)+"</td>"+ 
-                                "<td class='snippet-content' type='"+(j+1)+"'>"+lineDataSource[j]+"</td>"+
-                           "</tr>";
+            if( text.indexOf("return") >　-1) {
+                dictText = text.substr(text.indexOf("return"),6)
+                snippetContent[i].innerHTML = text.replace(dictText, "<i style='color:#db73e2;'>return</i>");
             }
-            content +="</table>";
-            snippetTag[i].innerHTML = content;
         }
-    } 
+    }
 }
